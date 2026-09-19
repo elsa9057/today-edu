cd9afeb2b95dd8a0f3e103c3aa0f7233031b68cb
elsa9057 <sjoebs@gmail.com>
2026-09-19 21:12:38 +0900

    Add files via upload

--- collector/server.ps1
+++ collector/server.ps1
@@ -91,7 +91,10 @@ function Clean-EduNoise([string]$text) {
   if ([string]::IsNullOrWhiteSpace($text)) { return '' }
   return ($text -replace $EduNoiseRegex, ' ')
 }
+# 드라마·예능 등 연예 기사는 '교사', '학교'가 나와도 교육 뉴스에서 뺍니다.
+$EntertainRegex = '(드라마|로맨스|시청률|주연|예능|캐스팅|OST|웹툰|뮤지컬|아이돌|팬미팅|컴백|첫\s?방송|방송\s?예정|극본|열연|안방극장)'
 function Test-Education([string]$text) {
+  if ($text -match $EntertainRegex) { return $false }
   return ((Clean-EduNoise $text) -match $EducationRegex)
 }
 
